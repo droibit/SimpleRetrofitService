fdcf8e65e573d7294632228ad7fa2d7efa3f0a64
Shinya Kumagai <roomful.rooms@gmail.com>
2016-02-21 22:00:15 +0900

    リクエスト内の日付フォーマットを修正

--- app/src/main/java/com/github/droibit/simpleretrofitservice/net/ConnpassClient.kt
+++ app/src/main/java/com/github/droibit/simpleretrofitservice/net/ConnpassClient.kt
@@ -7,10 +7,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.QueryMap
+import java.text.SimpleDateFormat
 import java.util.*
 
 private val connpassApiUrl = "http://connpass.com"
 private val defaultReturnCount = 10
+
+private val ymFormat by lazy { SimpleDateFormat("yyyyMM") }
+private val ymdFormat by lazy { SimpleDateFormat("yyyyMMdd") }
 
 /**
  *
@@ -23,17 +28,21 @@ class ConnpassClient {
 
         @GET("/api/v1/event/")
         fun searchKeyword(@Query("keyword") keyword: String,
-                          @Query("ym") ymDates: List<@JvmSuppressWildcards Date>? = null,
+                          @Query("ymd") ymdDates: List<@JvmSuppressWildcards String>? = null,
                           @Query("order") order: Order = Order.EVENT,
                           @Query("start") start: Int = 0,
                           @Query("count") count: Int = defaultReturnCount): Call<EventResponse>
 
         @GET("/api/v1/event/")
         fun searchEventId(@Query("event_id") eventId: String,
-                          @Query("ymd") ymdDates: List<@JvmSuppressWildcards Date>?,
+                          @Query("ymd") ymdDates: List<@JvmSuppressWildcards String>? = null,
                           @Query("order") order: Order = Order.EVENT,
-                          @Query("start") start: Int,
-                          @Query("count") count: Int): Call<EventResponse>
+                          @Query("start") start: Int = 0,
+                          @Query("count") count: Int = defaultReturnCount): Call<EventResponse>
+
+        /** @hide */
+        @GET("/api/v1/event/")
+        fun search(@QueryMap query: Map<String, @JvmSuppressWildcards Any?>): Call<EventResponse>
     }
 
     @JvmField
@@ -54,13 +63,25 @@ class ConnpassClient {
     }
 }
 
-fun ConnpassClient.Service.searchEventId(eventId: String,
-                                         ymDates: List<@JvmSuppressWildcards Date>? = null,
-                                         order: Int = Order.EVENT.index,
+fun ConnpassClient.Service.searchKeyword(keyword: String,
+                                         ymdDates: List<@JvmSuppressWildcards Date>? = null,
+                                         order: Order = Order.EVENT,
                                          start: Int = 0,
                                          count: Int = defaultReturnCount): Call<EventResponse> {
-    return searchEventId(eventId, ymDates, order, start, count)
+    val dates = ymdDates?.map { ymdFormat.format(it) }
+    return searchEventId(keyword, dates, order, start, count)
 }
+
+fun ConnpassClient.Service.searchEventId(eventId: String,
+                                         ymdDates: List<@JvmSuppressWildcards Date>? = null,
+                                         order: Order = Order.EVENT,
+                                         start: Int = 0,
+                                         count: Int = defaultReturnCount): Call<EventResponse> {
+    val dates = ymdDates?.map { ymdFormat.format(it) }
+    return searchEventId(eventId, dates, order, start, count)
+}
+
+
 
 enum class Order(val index: Int) {
     UPDATED(0), // 更新日時順
--- app/src/test/java/com/github/droibit/simpleretrofitservice/net/ConnpassClientTest.kt
+++ app/src/test/java/com/github/droibit/simpleretrofitservice/net/ConnpassClientTest.kt
@@ -2,12 +2,14 @@ package com.github.droibit.simpleretrofitservice.net
 
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.Assert.assertThat
+import java.util.*
 
 /**
  *
@@ -27,6 +29,11 @@ class ConnpassClientTest {
         connpassClient = ConnpassClient(baseUrl = "${mockServer.url("/")}")
     }
 
+    @After
+    fun tearDown() {
+        mockServer.shutdown()
+    }
+
     @Test
     fun searchKeyword() {
         val service = connpassClient.service
@@ -41,12 +48,30 @@ class ConnpassClientTest {
 
         println(response.raw())
         assertThat(response.body(), notNullValue())
-        assertThat(response.body().events.size, CoreMatchers.`is`(3))
+        assertThat(response.body().events.size, `is`(3))
+    }
+
+    @Test
+    fun searchKeywordExt() {
+        val service = connpassClient.service
+
+        val mockResponse = MockResponse().apply {
+            setBody(mockMultiEventResponse)
+        }
+        mockServer.enqueue(mockResponse)
+
+        val call = service.searchKeyword("hoge", ymdDates = listOf(Date(), Date()))
+        val response = call.execute()
+
+        println(response.raw())
+        assertThat(response.body(), notNullValue())
+        assertThat(response.body().events.size, `is`(3))
     }
 
     @Test
     fun searchEventId() {
         val service = connpassClient.service
+        service.search(emptyMap())
 
         val mockResponse = MockResponse().apply {
             setBody(mockMultiEventResponse)
@@ -58,6 +83,23 @@ class ConnpassClientTest {
 
         println(response.raw())
         assertThat(response.body(), notNullValue())
-        assertThat(response.body().events.size, CoreMatchers.`is`(3))
+        assertThat(response.body().events.size, `is`(3))
+    }
+
+    @Test
+    fun searchEventIdExt() {
+        val service = connpassClient.service
+
+        val mockResponse = MockResponse().apply {
+            setBody(mockMultiEventResponse)
+        }
+        mockServer.enqueue(mockResponse)
+
+        val call = service.searchEventId("hoge", ymdDates = listOf(Date(), Date()))
+        val response = call.execute()
+
+        println(response.raw())
+        assertThat(response.body(), notNullValue())
+        assertThat(response.body().events.size, `is`(3))
     }
 }
